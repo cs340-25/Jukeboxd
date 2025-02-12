dd921766b4543e8bb2557ce92f7d7b945c50058e
Alex Chen <143532913+alexchen1337@users.noreply.github.com>
2025-02-12 17:02:39 -0500

    custom scroll bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './pages/Home'
+import CustomScrollbar from './components/CustomScrollbar'
 import './styles/App.css'
 
 function App() {
   return (
     <Router>
+      <CustomScrollbar />
       <div className="app">
         <Navbar />
         <Routes>
