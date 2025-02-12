c9319c129da16bef1f995ee64b7904ca8e0a6001
Alex Chen <143532913+alexchen1337@users.noreply.github.com>
2025-02-12 17:16:21 -0500

    favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Add more routes as needed */}
         </Routes>
       </div>
     </Router>
