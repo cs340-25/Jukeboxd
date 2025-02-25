4497c19160f54fc6e3e747598f59b4ccdfd5532e
ethandietrich00808 <edietri2@vols.utk.edu>
2025-02-25 13:47:18 -0500

    Changes made for hosting

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from flask import Flask
+from flask_cors import CORS
 
-app = create_app()
+app = Flask(__name__)
+# Update CORS to allow connections from any origin
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
